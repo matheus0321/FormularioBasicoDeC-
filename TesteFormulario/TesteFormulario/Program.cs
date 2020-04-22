bcb85c60eee02316b1fac7f5d9dad529138723a2
matheus0321 <mtbveloso0321@gmail.com>
2020-04-22 14:58:40 -0300

    Add files via upload

---
+++ TesteFormulario/TesteFormulario/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TesteFormulario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seja Bem Vindo!");
+            string nome, cidade, numero, sexo;
+
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Digite sua cidade: ");
+            cidade = Console.ReadLine();
+
+          Console.Write("Digite seu numero de Telefone: ");
+           numero = Console.ReadLine();
+
+            Console.Write("Digite M para Masculino e F para Feminino: ");
+            sexo = Console.ReadLine();
+            
+            Console.WriteLine("Ola {0} , você esta morando em  {1} , e seu numero de telefone para contato é  esse {2} , e você e do sexo {3}",nome,cidade,numero,sexo);
+            
+            Console.ReadKey(); 
+        }
+    }
+}
